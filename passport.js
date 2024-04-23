3c57747228784073c1f5187785fc8fc3911a4ccf
Malgorzata Staszewska-Bui <136642341+staszewska@users.noreply.github.com>
2024-04-23 17:56:54 +0200

    authentication strategies: “LocalStrategy” and “JWTStrategy.” defined

---
+++ passport.js
@@ -0,0 +1,55 @@
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const Models = require("./models");
+const passportJWT = require("passport-jwt");
+
+let Users = Models.User,
+  JWTStrategy = passportJWT.Strategy,
+  ExtractJWT = passportJWT.ExtractJwt;
+
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: "Username",
+      passwordField: "Password",
+    },
+    async (username, password, callback) => {
+      console.log(`${username} ${password}`);
+      await Users.findOne({ Username: username });
+    }
+  )
+    .then((user) => {
+      if (!user) {
+        console.log("incorrect username");
+        return callback(null, false, {
+          message: "Incorrect username or password",
+        });
+      }
+      console.log("finished");
+      return callback(null, user);
+    })
+    .catch((error) => {
+      if (error) {
+        console.log(error);
+        return callback(error);
+      }
+    })
+);
+
+passport.use(
+  new JWTStrategy(
+    {
+      jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerTotek(),
+      secretKey: "your_jwt_secret",
+    },
+    async (jwtPayload, callback) => {
+      return await Users.findById(jwtPayload._id)
+        .then((user) => {
+          return callback(null, user);
+        })
+        .catch((error) => {
+          return callback(error);
+        });
+    }
+  )
+);
