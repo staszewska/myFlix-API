781deea7cd898cf95bab340c410e3323facff514
Malgorzata Staszewska-Bui <136642341+staszewska@users.noreply.github.com>
2024-04-24 00:32:30 +0200

    authentication added to endpoints

--- index.js
+++ index.js
@@ -132,46 +132,58 @@ app.get(
 );
 
 //Return data (description, genre, director, image URL, whether it’s featured or not) about a single movie by title to the user
-app.get("/movies/:title", async (request, response) => {
-  // console.log("Title is: ", request.params.title);
-  await Movies.findOne({ Title: request.params.title })
-    .then((movie) => {
-      response.json(movie);
-    })
-    .catch((error) => {
-      response.status(500).send("Error: " + error);
-    });
-});
+app.get(
+  "/movies/:title",
+  passport.authenticate("jwt", { session: false }),
+  async (request, response) => {
+    // console.log("Title is: ", request.params.title);
+    await Movies.findOne({ Title: request.params.title })
+      .then((movie) => {
+        response.json(movie);
+      })
+      .catch((error) => {
+        response.status(500).send("Error: " + error);
+      });
+  }
+);
 
 //Return data about a genre (description) by name/title
-app.get("/movies/genre/:genreName", async (request, response) => {
-  const genreName = request.params.genreName;
+app.get(
+  "/movies/genre/:genreName",
+  passport.authenticate("jwt", { session: false }),
+  async (request, response) => {
+    const genreName = request.params.genreName;
 
-  await Movies.findOne({ "Genre.Name": genreName })
+    await Movies.findOne({ "Genre.Name": genreName })
 
-    .then((movie) => {
-      response.json(movie.Genre);
-    })
+      .then((movie) => {
+        response.json(movie.Genre);
+      })
 
-    .catch((error) => {
-      response.status(500).send("Error: " + error);
-    });
-});
+      .catch((error) => {
+        response.status(500).send("Error: " + error);
+      });
+  }
+);
 
 //Return data about a director
-app.get("/movies/directors/:directorName", async (request, response) => {
-  const directorName = request.params.directorName;
+app.get(
+  "/movies/directors/:directorName",
+  passport.authenticate("jwt", { session: false }),
+  async (request, response) => {
+    const directorName = request.params.directorName;
 
-  await Movies.findOne({ "Director.Name": directorName })
+    await Movies.findOne({ "Director.Name": directorName })
 
-    .then((movie) => {
-      response.json(movie.Director);
-    })
+      .then((movie) => {
+        response.json(movie.Director);
+      })
 
-    .catch((error) => {
-      response.status(500).send("Error: ", error);
-    });
-});
+      .catch((error) => {
+        response.status(500).send("Error: ", error);
+      });
+  }
+);
 
 //POST requests (CREATE)
 
@@ -207,88 +219,128 @@ app.post("/users", async (request, response) => {
 //PUT requests (UPDATE)
 
 //Allow users to update their user info
-app.put("/users/:name", async (request, response) => {
-  console.log("User name:", request.params.name);
-  await Users.findOneAndUpdate(
-    { Name: request.params.name },
+app.put(
+  "/users/:name",
+  passport.authenticate("jwt", { session: false }),
+  async (request, response) => {
+    console.log("request.user.Name", request.user.Name);
+    console.log("request.params.name", request.params.name);
 
-    {
-      $set: {
-        Name: request.body.Name,
-        Email: request.body.Email,
-        Birthday: request.body.Birthday,
-        Country: request.body.Country,
-        Gender: request.body.Gender,
+    // CONDITION TO CHECK ADDED HERE
+    if (request.user.Name !== request.params.name) {
+      return response.status(400).send("Permission denied");
+    }
+
+    // CONDITION ENDS
+    await Users.findOneAndUpdate(
+      { Name: request.params.name },
+
+      {
+        $set: {
+          Name: request.body.Name,
+          Email: request.body.Email,
+          Birthday: request.body.Birthday,
+          Country: request.body.Country,
+          Gender: request.body.Gender,
+        },
       },
-    },
-    { new: true }
-  )
+      { new: true }
+    )
 
-    .then((updatedUser) => {
-      response.json(updatedUser);
-    })
-    .catch((error) => {
-      response(500).send("Error: ", error);
-    });
-});
+      .then((updatedUser) => {
+        response.json(updatedUser);
+      })
+      .catch((error) => {
+        response(500).send("Error: ", error);
+      });
+  }
+);
 
 // update users favorite movie
-app.put("/users/:name/movies/:movieID", async (request, response) => {
-  await Users.findOneAndUpdate(
-    { Name: request.params.name },
-    {
-      $push: { favoriteMovies: request.params.movieID },
-    },
-    { new: true }
-  )
+app.put(
+  "/users/:name/movies/:movieID",
+  passport.authenticate("jwt", { session: false }),
+  async (request, response) => {
+    // CONDITION TO CHECK ADDED HERE
+    if (request.user.Name !== request.params.name) {
+      return response.status(400).send("Permission denied");
+    }
 
-    .then((updatedUser) => {
-      response.json(updatedUser);
-    })
+    await Users.findOneAndUpdate(
+      { Name: request.params.name },
 
-    .catch((error) => {
-      response.status(500).send("Error: ", error);
-    });
-});
+      {
+        $push: { favoriteMovies: request.params.movieID },
+      },
+      { new: true }
+    )
+
+      .then((updatedUser) => {
+        response.json(updatedUser);
+      })
+
+      .catch((error) => {
+        response.status(500).send("Error: ", error);
+      });
+  }
+);
 
 //DELETE requests
 
 //Allow users to remove a movie from their list of favorites
-app.delete("/users/:name/movies/:movieID", async (request, response) => {
-  await Users.findOneAndUpdate(
-    { Name: request.params.name },
-    {
-      $pull: { favoriteMovies: request.params.movieID },
-    },
-    { new: true }
-  )
+app.delete(
+  "/users/:name/movies/:movieID",
+  passport.authenticate("jwt", { session: false }),
+  async (request, response) => {
+    // CONDITION TO CHECK ADDED HERE
+    if (request.user.Name !== request.params.name) {
+      return response.status(400).send("Permission denied");
+    }
 
-    .then((updatedUser) => {
-      response.json(updatedUser);
-    })
+    await Users.findOneAndUpdate(
+      { Name: request.params.name },
+      {
+        $pull: { favoriteMovies: request.params.movieID },
+      },
+      { new: true }
+    )
 
-    .catch((error) => {
-      response.status(500).send("Error: ", error);
-    });
-});
+      .then((updatedUser) => {
+        response.json(updatedUser);
+      })
+
+      .catch((error) => {
+        response.status(500).send("Error: ", error);
+      });
+  }
+);
 
 //Allow existing users to deregister
-app.delete("/users/:name", async (request, response) => {
-  console.log("Name is: ", request.params.name);
-  await Users.findOneAndDelete({ Name: request.params.name })
+app.delete(
+  "/users/:name",
+  passport.authenticate("jwt", { session: false }),
+  async (request, response) => {
+    // CONDITION TO CHECK ADDED HERE
+    if (request.user.Name !== request.params.name) {
+      return response.status(400).send("Permission denied");
+    }
 
-    .then((user) => {
-      if (!user) {
-        response.status(400).send(request.params.name + " was not found");
-      } else {
-        response.status(200).send(request.params.name + " was deleted");
-      }
-    })
+    console.log("Name is: ", request.params.name);
+    await Users.findOneAndDelete({ Name: request.params.name })
 
-    .catch((error) => {
-      response.status(500).send("Error: ", error);
-    });
-});
+      .then((user) => {
+        if (!user) {
+          response.status(400).send(request.params.name + " was not found");
+        } else {
+          response.status(200).send(request.params.name + " was deleted");
+        }
+      })
+
+      .catch((error) => {
+        response.status(500).send("Error: ", error);
+      });
+  }
+);
 
 // This works but we can also use express.static to serve static files
 // app.get("/documentation", (req, res) => {
