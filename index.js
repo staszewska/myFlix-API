9450915d4d99c2b358e8ac6f770d09d7d320c013
Malgorzata Staszewska-Bui <136642341+staszewska@users.noreply.github.com>
2024-04-26 21:54:09 +0200

    use correct ENV variable

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const Users = Models.User;
 //   useUnifiedTopology: true,
 // });
 
-mongoose.connect("process.env.CONNECTION_URI", {
+mongoose.connect(process.env.CONNECTION_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
